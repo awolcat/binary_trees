e99d24e8add736ce93d2900b22680487f9477677
Albert <albertirura11@gmail.com>
2023-08-02 23:23:04 +0300

    height of a binary tree

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,63 @@
+#include <stddef.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+size_t log_base_two(size_t height);
+size_t count(const binary_tree_t *tree, size_t height);
+/**
+ * binary_tree_height - get the height of a binary tree
+ *
+ * @tree: pointer to the root of the tree
+ *
+ * Return: size_t, height of the tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t height;
+	size_t true_height;
+
+	if (!tree)
+		return (0);
+
+	height = count(tree, 0);
+	true_height = log_base_two(height);
+
+	return (true_height);
+}
+
+/**
+ * count - count the approximate number of nodes
+ *
+ * @tree: pointer to the root of the binary tree
+ * @height: number of nodes
+ *
+ * Return: size_t, number of nodes
+ */
+
+size_t count(const binary_tree_t *tree, size_t height)
+{
+	if (!tree)
+		return (height);
+	height = count(tree->left, height + 1);
+	height = count(tree->right, height + 1);
+	return (height);
+}
+
+/**
+ * log_base_two - find the height given the approx node count
+ *
+ * @height: approximate number of nodes in the tree
+ *
+ * Return: size_t, height of the tree
+ */
+size_t log_base_two(size_t height)
+{
+	size_t true_height = 0;
+
+	while (height >= 1)
+	{
+		height = height / 2;
+		true_height += 1;
+	}
+	return (true_height - 2);
+}
