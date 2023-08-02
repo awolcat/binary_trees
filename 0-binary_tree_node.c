2506b000f44d39a4824b74798d6d8b2a5a39944e
Albert <albertirura11@gmail.com>
2023-08-02 13:27:54 +0300

    binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -19,5 +19,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		return (node);
 	node->n = value;
 	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
 	return (node);
 }
