e043a10a8cfbe239cb806ae89eaf3ddc0429ab22
Albert <albertirura11@gmail.com>
2023-08-03 18:45:15 +0300

    count leaves of a binary tree

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -2,44 +2,19 @@
 #include "binary_trees.h"
 
 /**
- * traverse_tree - return each node of a tree
+ * binary_tree_leaves - count leaves of a binary tree
  *
- * @tree: root of the tree/subtree
- * @leaves: number of nodes with no children
+ * @tree: root of the tree
  *
  * Return: size_t, number of leaves
- */
-size_t traverse_tree(const binary_tree_t *tree, size_t leaves)
-{
-	size_t flag = 0;
-
-	if (!tree)
-		return (0);
-	flag = traverse_tree(tree->left, leaves);
-	leaves += flag;
-	flag = traverse_tree(tree->right, leaves);
-	leaves += flag;
-	if (!tree->left && !tree->right)
-		return (1);
-	return (leaves);
-}
-
-/**
- * binary_tree_leaves - wrapper for traverse_tree
- *
- * @tree: root of the tree/subtree
- *
- * Return: size_t, number of nodes
  */
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t leaves = 0;
-
 	if (!tree)
 		return (0);
-	leaves = traverse_tree(tree, leaves);
-	if (!tree->parent)
-		leaves -= 1;
-	return (leaves);
+	if (!tree->left && !tree->right)
+		return (1);
+	return (binary_tree_leaves(tree->left)
+		+ binary_tree_leaves(tree->right));
 }
